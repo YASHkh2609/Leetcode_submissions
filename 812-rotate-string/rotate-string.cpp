a8219d5ad4bd18dac0540501b8c3764a37864c5b
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2023-08-29 02:29:38 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (63.11%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        int i=0;
-        int j=0;
-        string ans=s+s;
-        while(ans.size()-i>goal.size()-j){
-            int temp=i;
-            while(ans[temp]==goal[j] && j<goal.size()){
-                temp++;
-                j++;
-            }
-            if(j==goal.size() && s.size()==goal.size()){
-                return true;
-            }
-            else
-                j=0;
-            i++;
+        // int i=0;
+        // int j=0;
+        // string ans=s+s;
+        // while(ans.size()-i>goal.size()-j){
+        //     int temp=i;
+        //     while(ans[temp]==goal[j] && j<goal.size()){
+        //         temp++;
+        //         j++;
+        //     }
+        //     if(j==goal.size() && s.size()==goal.size()){
+        //         return true;
+        //     }
+        //     else
+        //         j=0;
+        //     i++;
+        // }
+        // return false;
+        if(s.size()!=goal.size()) return false;
+
+        string concatenate = s+s;
+        if(concatenate.find(goal)!= string::npos){
+            return true;
         }
-        return false;
+        else return false;
     }
 };
