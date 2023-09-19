1bd88c69bf652eecc16c798d7e5f297d48a071a1
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2023-09-20 02:20:28 +0530

    Time: 7 ms (77.51%) | Memory: 7.6 MB (9.58%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,25 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(t.begin(),t.end());
-        sort(s.begin(),s.end());
-        if(t==s)return true;
-        else return false;
+        // sort(t.begin(),t.end());
+        // sort(s.begin(),s.end());
+        // if(t==s)return true;
+        // else return false;
+        int freq_arr1[26]={0};
+        int freq_arr2[26]={0};
+        if(s.length()==t.length()){
+            for(int i=0;i<s.length();i++){
+                freq_arr1[int(s[i])-int('a')]++;
+                freq_arr2[int(t[i])-int('a')]++;
+            }
+            for(int i=0;i<26;i++){
+                if(freq_arr1[i]!=freq_arr2[i])
+                    return false;
+            }
+            return true;
+        }
+        else
+            return false;
+        return 0;
     }
 };
