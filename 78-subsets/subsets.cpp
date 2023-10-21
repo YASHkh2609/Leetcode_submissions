12616abf03929c1ef9dd0eb5f633b7acf0bc9843
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2023-10-22 00:58:35 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (48.80%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int n=nums.size();
+        for(int i=0; i< pow(2,n); i++){
+            vector<int>v;
+            for(int j=0;j<n;j++){
+                if(i & 1<<j){//j th bit set or not in i
+                    v.push_back(nums[j]);
+                }
+            }
+            ans.push_back(v);
+        }
+        return ans;
+    }
+};
