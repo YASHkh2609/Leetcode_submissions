4dddd849f5a59dccc6bd9d7863a6b82595e4bd92
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2023-10-26 11:59:03 +0530

    Time: 4 ms (44.21%) | Memory: 7.9 MB (65.26%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -16,11 +16,23 @@ public:
             }
         }
     }
+    void recurPermute2(vector<int>& nums,int ind,vector<vector<int>>&ans){
+        if(ind==nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=ind;i<nums.size();i++){
+            swap(nums[i],nums[ind]);
+            recurPermute2(nums,ind+1,ans);
+            swap(nums[i],nums[ind]);
+        }
+    }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>ans;
-        vector<int>ds;
-        int freq[21]={0};
-        recurPermute1(nums,ds,ans,freq);
+        //vector<int>ds;
+        //int freq[21]={0};
+        //recurPermute1(nums,ds,ans,freq);
+        recurPermute2(nums,0,ans);
         return ans;
     }
 };
