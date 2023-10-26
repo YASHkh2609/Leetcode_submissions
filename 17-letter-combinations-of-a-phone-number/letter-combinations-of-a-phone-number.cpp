0aaef9c3d526ed087d177a1eaa25d795728c70de
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2023-10-26 10:53:57 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (21.49%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void getCombination(string digits, vector<string>&ans,string keys[], string str, int i){
+        if(digits.length()==0){
+            return;
+        }
+        if(i==digits.length()){
+            ans.push_back(str);
+            return;
+        }
+        int digit=digits[i]-'0';
+        for(int k=0;keys[digit][k]!='\0';k++){
+            
+            str+=keys[digit][k];
+            getCombination(digits, ans, keys, str, i+1);
+            str.pop_back();
+        }
+
+    }
+    vector<string> letterCombinations(string digits) {
+        string keys[]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        vector<string>ans;
+        string str;
+        getCombination(digits,ans,keys, str,0);
+        return ans;
+    }
+};
