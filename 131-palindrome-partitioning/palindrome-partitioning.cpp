f9a93255056f4adcee6c378f3f2460619904991b
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2023-10-28 11:44:23 +0530

    Time: 107 ms (49.11%) | Memory: 58.1 MB (44.63%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,11 +1,8 @@
-/*class Solution {
+class Solution {
 public:
     bool isPalindrome(int j, int k, string str){
-        while(j<=k){
-            if(str[j]!=str[k])return false;
-            j++;
-            k--;
-        }
+        while(j<k)
+            if(str[j++]!=str[k--])return false;
         return true;
     }
     void doPartition(string s,int ind,vector<string>&ds, vector<vector<string>>&ans ){
@@ -27,40 +24,5 @@ public:
         vector<vector<string>>ans;
         doPartition(s,0,ds,ans);
         return ans;
-    }
-};*/
-class Solution {
-public:
-    vector<vector<string>> partition(string s) {
-        vector<vector<string>> res;
-        vector<string> path;
-        func(0,s,path,res);
-        return res;
-    }
-
-    void func(int index, string s, vector<string> &path, vector<vector<string>> &res){
-        if(index == s.size()){
-            res.push_back(path);
-            return;
-        }
-
-        for(int i=index;i<s.size();i++){
-            //Check if next indices forms a palindrome
-            if(isPalindrome(s,index,i)){
-                path.push_back(s.substr(index,i-index+1));
-                //Check if index can be further divided into palindrome
-                func(i+1,s,path,res);
-                //Backtracking
-                path.pop_back();
-            }
-        }
-    }
-    
-    bool isPalindrome(string s,int start,int end){
-        //Checks for palindrome
-        while(start <= end){
-            if(s[start++] != s[end--]) return false;
-        }
-        return true;
     }
 };
