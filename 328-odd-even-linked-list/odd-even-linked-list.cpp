0c4c241d1bdb280dcb3fbd8a30d76b68508904a6
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2023-11-22 19:11:18 +0530

    Time: 10 ms (28.94%) | Memory: 10.8 MB (39.62%) - LeetSync

---
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void PutAtEnd(ListNode* &head, ListNode* &prev, ListNode* &tail){
+        ListNode* cur=prev->next;
+        prev->next=cur->next;
+        cur->next=NULL;
+        prev=prev->next;
+        tail->next=cur;
+        tail=tail->next;
+        return;
+    }
+    ListNode* oddEvenList(ListNode* head) {
+        if(head==NULL || head->next==NULL || head->next->next==NULL)return head;
+        ListNode* tail=head;
+        while(tail->next!=NULL){
+            tail=tail->next;
+        }
+        ListNode* temp=head;
+        ListNode* second_node=head->next;
+        PutAtEnd(head,temp,tail);
+        while(temp!=second_node && temp->next!=second_node){
+            PutAtEnd(head,temp,tail);
+        }
+        return head;
+    }
+};
