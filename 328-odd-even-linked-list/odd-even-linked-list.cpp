a7d0b6e01a7ee1335825916b618f8c708f2cf91f
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2023-11-22 19:57:54 +0530

    Time: 7 ms (76.23%) | Memory: 10.7 MB (71.74%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -21,16 +21,29 @@ public:
     }
     ListNode* oddEvenList(ListNode* head) {
         if(head==NULL || head->next==NULL || head->next->next==NULL)return head;
-        ListNode* tail=head;
-        while(tail->next!=NULL){
-            tail=tail->next;
+        // ListNode* tail=head;
+        // while(tail->next!=NULL){
+        //     tail=tail->next;
+        // }
+        // ListNode* temp=head;
+        // ListNode* second_node=head->next;
+        // PutAtEnd(head,temp,tail);
+        // while(temp!=second_node && temp->next!=second_node){
+        //     PutAtEnd(head,temp,tail);
+        // }
+        // return head;
+        ListNode* odd=head;
+        ListNode* even=head->next;
+        ListNode* evenhead=even;
+
+        while(even!=NULL && even->next!=NULL){
+            odd->next=odd->next->next;
+            odd=odd->next;
+
+            even->next=even->next->next;
+            even=even->next;
         }
-        ListNode* temp=head;
-        ListNode* second_node=head->next;
-        PutAtEnd(head,temp,tail);
-        while(temp!=second_node && temp->next!=second_node){
-            PutAtEnd(head,temp,tail);
-        }
+        odd->next=evenhead;
         return head;
     }
 };
