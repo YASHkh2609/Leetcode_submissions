03d7fc03e482e108d8006b29fda09e75d79e5f95
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2024-02-08 01:55:30 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (36.11%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        int orr = start|goal;
-        int andd = start&goal;
+        // int orr = start|goal;
+        // int andd = start&goal;
         int cnt=0;
-        while(orr!=0 || andd!=0){
-            if( (orr&1) != (andd&1)){
-                cnt++;
-                // cout<<orr<<" "<<andd<< " ";
-                // cout<< cnt<<endl;
-            }
-            orr=orr>>1;
-            andd=andd>>1;
+        // while(orr!=0 || andd!=0){
+        //     if( (orr&1) != (andd&1)){
+        //         cnt++;
+        //         // cout<<orr<<" "<<andd<< " ";
+        //         // cout<< cnt<<endl;
+        //     }
+        //     orr=orr>>1;
+        //     andd=andd>>1;
+        // }
+        int xorr= start ^ goal; // xor will set only the bits which are flipped
+        //now we can count the set bits
+        while(xorr!=0){
+            if((xorr&1)==1)cnt++;
+            xorr=xorr>>1;
         }
+
         return cnt;
     }
 };
