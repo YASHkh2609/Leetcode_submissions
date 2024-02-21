79cba1a1eac175db4a27914cb75358f2a742ae0c
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2024-02-21 13:10:24 +0530

    Time: 4 ms (51.26%) | Memory: 17.3 MB (21.98%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -11,34 +11,13 @@
  */
 class Solution {
 public:
-    void Inorder1(TreeNode* root, vector<int>& traversal){
-        if(root==NULL){
-            traversal.push_back(-101);
-            return;
-        }
-        traversal.push_back(root->val);
-        Inorder1(root->left, traversal);
-        Inorder1(root->right, traversal);
-    }
-    void Inorder2(TreeNode* root, vector<int>& traversal){
-        if(root==NULL){
-            traversal.push_back(-101);
-            return;
-        }
-        traversal.push_back(root->val);
-        Inorder2(root->right, traversal);
-        Inorder2(root->left, traversal);
+    bool isSymmetricHelp(TreeNode* left, TreeNode* right){
+        if(left==NULL || right ==NULL)return left==right;
+        if(left->val!=right->val)return false;
+
+        return isSymmetricHelp(left->left, right->right) && isSymmetricHelp(left->right, right->left);
     }
     bool isSymmetric(TreeNode* root) {
-        if(root->left==NULL && root->right==NULL)return true;
-
-        vector<int> left, right;
-        Inorder1(root->left, left);
-        Inorder2(root->right, right);
-
-        for(int i=0;i<left.size();i++){
-            if(left[i]!=right[i])return false;
-        }
-        return true;
+        return root==NULL || isSymmetricHelp(root->left, root->right) ;
     }
 };
