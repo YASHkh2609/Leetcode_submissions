580a6203291accc239e15ee8430c3999b45fda7e
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2024-02-26 13:12:12 +0530

    Time: 74 ms (23.74%) | Memory: 56 MB (93.82%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.cpp
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.cpp
@@ -13,24 +13,41 @@ class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
         TreeNode * NodeToInsert=new TreeNode(val);
-        if(root==NULL)return NodeToInsert;
-        TreeNode * prev=NULL, * cur=root;
-        bool flag=1;
-        while(cur){
-            if(val<cur->val){
-                prev=cur;
-                cur=cur->left;
-                flag=0;
-            }
-            else{
-                prev=cur;
-                cur=cur->right;
-                flag=1;
-            }
+         if(root==NULL)return NodeToInsert;
+        // TreeNode * prev=NULL, * cur=root;
+        // bool flag=1;
+        // while(cur){
+        //     if(val<cur->val){
+        //         prev=cur;
+        //         cur=cur->left;
+        //         flag=0;
+        //     }
+        //     else{
+        //         prev=cur;
+        //         cur=cur->right;
+        //         flag=1;
+        //     }
 
-        } 
-        if(flag==0)prev->left=NodeToInsert;
-        else prev->right=NodeToInsert;
+        // } 
+        // if(flag==0)prev->left=NodeToInsert;
+        // else prev->right=NodeToInsert;
+        TreeNode* cur=root;
+        while(true){
+            if(val<cur->val){ //check for left
+                if(cur->left!=NULL)cur=cur->left;//if not leaf, go left
+                else{
+                    cur->left=NodeToInsert;//if leaf, insert
+                    break;
+                }
+            }
+            else{//check for right
+                if(cur->right!=NULL)cur=cur->right;//if not leaf, go right
+                else{
+                    cur->right=NodeToInsert;//if leaf, insert
+                    break;
+                }
+            }
+        }
         return root;
     }
 };
