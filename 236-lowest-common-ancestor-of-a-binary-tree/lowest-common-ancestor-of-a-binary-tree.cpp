41a91342639f8405f14d50bf4c379665af322a6b
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2024-03-01 12:37:56 +0530

    Time: 19 ms (14.07%) | Memory: 16 MB (58.77%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,29 +9,36 @@
  */
 class Solution {
 public:
-    bool get_path(TreeNode* root, TreeNode* node, vector<TreeNode* >&path){
-        if(!root)return false;
+    // bool get_path(TreeNode* root, TreeNode* node, vector<TreeNode* >&path){
+    //     if(!root)return false;
 
-        path.push_back(root);
-        if(root==node)return true;
+    //     path.push_back(root);
+    //     if(root==node)return true;
 
-        if(get_path(root->left, node, path) || get_path(root->right, node, path))return true;
+    //     if(get_path(root->left, node, path) || get_path(root->right, node, path))return true;
 
-        path.pop_back();
-        return false;
+    //     path.pop_back();
+    //     return false;
 
-    }
+    // }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode* >pPath,qPath;
+        // vector<TreeNode* >pPath,qPath;
 
-        bool path1=get_path(root, p, pPath);
-        bool path2=get_path(root, q, qPath);
+        // bool path1=get_path(root, p, pPath);
+        // bool path2=get_path(root, q, qPath);
 
-        TreeNode * ans;
-        for(int i=0;i<min(pPath.size(), qPath.size());i++){
-            if(pPath[i]==qPath[i])ans=pPath[i];
-        }
-        return ans;
+        // TreeNode * ans;
+        // for(int i=0;i<min(pPath.size(), qPath.size());i++){
+        //     if(pPath[i]==qPath[i])ans=pPath[i];
+        // }
+        // return ans;
+        if(root==NULL || root==p|| root==q)return root;
+        TreeNode* left=lowestCommonAncestor(root->left, p,q);
+        TreeNode* right=lowestCommonAncestor(root->right, p, q);
+
+        if(left==NULL)return right;
+        else if(right==NULL)return left;
+        return root;
         
     }
 };
