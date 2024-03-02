28b7e9cfeca03267febce0849896fb01903e0f6e
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2024-03-02 11:13:59 +0530

    Time: 19 ms (83.11%) | Memory: 29.4 MB (38.03%) - LeetSync

---
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int pos_index=nums.size();
+        int neg_index=-1;
+        if(nums[0]>=0){
+            pos_index=0;
+        }
+        else if(nums[nums.size()-1]<0){
+            neg_index=nums.size()-1;
+        }
+        else{
+            for(int i=0;i<nums.size()-1;i++){
+                if(nums[i]<0 && nums[i+1]>=0){
+                    neg_index=i;
+                    pos_index=i+1;
+                    break;
+                }
+            }
+        }
+        vector<int>ans;
+        
+        while(neg_index>=0 && pos_index<nums.size()){
+            if(nums[neg_index]*nums[neg_index]<=nums[pos_index]*nums[pos_index]){
+                ans.push_back(nums[neg_index]*nums[neg_index]);
+                neg_index--;
+            }
+            else{
+                ans.push_back(nums[pos_index]*nums[pos_index]);
+                pos_index++;
+            }
+        }
+        while(neg_index==-1 && pos_index<nums.size()){
+            ans.push_back(nums[pos_index]*nums[pos_index]);
+            pos_index++;
+        }
+        while(pos_index==nums.size() && neg_index>=0){
+            ans.push_back(nums[neg_index]*nums[neg_index]);
+            neg_index--;
+        }
+        return ans;
+    }
+};
