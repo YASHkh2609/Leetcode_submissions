c051b917ddd9f623dba44c8f72b1bbb81f1b3e7e
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2024-03-03 15:37:28 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (45.90%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -11,41 +11,10 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        //Brute Force TC: O(2N)
-        // ListNode* temp=head;
-        // int len=0;
-        // while(temp!=NULL){
-        //     len++;
-        //     temp=temp->next;
-        // }
-        // int pos=len-n+1;
-        // if(pos==1){
-        //     ListNode* temp1=head;
-        //     head=head->next;
-        //     temp1->next=NULL;
-        //     delete(temp1);
-        //     return head;
-        // }
-        // ListNode* prev=head;
-        // ListNode* cur=head->next;
-        // int cnt=2;
-        // while(cnt<pos){
-        //     prev=cur;
-        //     cur=cur->next;
-        //     cnt++;
-        // }
-        // prev->next=cur->next;
-        // cur->next=NULL;
-        // delete(cur);
-        // return head;
-        
-        //Optimal Solution
-        if(head->next==NULL)return NULL;
         ListNode* start=new ListNode();
         start->next=head;
-        ListNode* slow=start;
-        ListNode* fast=start;
         int cnt=0;
+        ListNode* fast=start, *slow=start;
         while(cnt<n){
             fast=fast->next;
             cnt++;
