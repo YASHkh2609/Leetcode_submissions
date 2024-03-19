14369d0a0f8022f207b3a611bebead8370f05c4e
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2024-03-19 11:17:25 +0530

    Time: 18 ms (81.95%) | Memory: 19.5 MB (28.04%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -16,6 +16,12 @@ public:
             }
         }
     }
+    void dfs(int node, vector<int>&vis, vector<vector<int>>&adj){
+        vis[node]=1;
+        for(auto it:adj[node]){
+            if(!vis[it])dfs(it, vis, adj);
+        }
+    }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n=isConnected.size();
         vector<vector<int>>adj(n);
@@ -31,7 +37,8 @@ public:
         int cnt=0;
         for(int i=0;i<n;i++){
             if(!vis[i]){
-                bfs(i, n, vis, adj);
+                //bfs(i, n, vis, adj);
+                dfs(i, vis, adj);
                 cnt++;
             }
         }
