f0c39f771d7ba5dd457dcad67e7be950117710e0
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2024-03-24 10:06:26 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (31.30%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -9,25 +9,27 @@
  */
 class Solution {
 private: 
-    TreeNode * find_node(TreeNode* root, TreeNode * target){
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()){
-            TreeNode * temp=q.front();
-            q.pop();
-            if(temp==target)return temp;
-            if(temp->left)q.push(temp->left);
-            if(temp->right)q.push(temp->right);
-        }
-        return NULL;
-    }
-    void get_parent_ptr(TreeNode* root, unordered_map<TreeNode*, TreeNode*>&parent_ptr){
+    // TreeNode * find_node(TreeNode* root, TreeNode * target){
+    //     queue<TreeNode*>q;
+    //     q.push(root);
+    //     while(!q.empty()){
+    //         TreeNode * temp=q.front();
+    //         q.pop();
+    //         if(temp==target)return temp;
+    //         if(temp->left)q.push(temp->left);
+    //         if(temp->right)q.push(temp->right);
+    //     }
+    //     return NULL;
+    // }
+    TreeNode* get_parent_ptr(TreeNode* root, unordered_map<TreeNode*, TreeNode*>&parent_ptr,TreeNode * target ){
         queue<TreeNode*>q;
         q.push(root);
         parent_ptr[root]=NULL;
+        TreeNode* newroot=NULL;
         while(!q.empty()){
             TreeNode* temp=q.front();
             q.pop();
+            if(temp==target)newroot=temp;
             if(temp->left){
                 q.push(temp->left);
                 parent_ptr[temp->left]=temp;
@@ -37,14 +39,15 @@ private:
                 parent_ptr[temp->right]=temp;
             }
         }
+        return newroot;
     }    
 public:
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
         vector<int>ans;
         unordered_map<TreeNode*, TreeNode*>parent_ptr;
-        get_parent_ptr(root, parent_ptr);
+        
 
-        TreeNode * newroot = find_node(root, target);
+        TreeNode * newroot = get_parent_ptr(root, parent_ptr, target);
         unordered_map<TreeNode*, bool>vis;
 
         queue<TreeNode*>q;
