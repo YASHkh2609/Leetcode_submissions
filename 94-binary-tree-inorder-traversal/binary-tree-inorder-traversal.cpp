3b53b2d7ce0f99f9621dc940e902b89f7c6cb0d6
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2024-03-27 18:48:49 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (38.88%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -11,16 +11,42 @@
  */
 class Solution {
 public:
-    void InOrder(TreeNode* root,vector<int>&ans){
-        if(root==NULL)return;
+    // void InOrder(TreeNode* root,vector<int>&ans){
+    //     if(root==NULL)return;
 
-        InOrder(root->left, ans);
-        ans.push_back(root->val);
-        InOrder(root->right, ans);
-    }
+    //     InOrder(root->left, ans);
+    //     ans.push_back(root->val);
+    //     InOrder(root->right, ans);
+    // }
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int>ans;
-        InOrder(root, ans);
+        // InOrder(root, ans);
+        // return ans;
+        
+        //Morris Traversal
+
+        TreeNode * cur=root;
+        while(cur!=NULL){
+            if(cur->left==NULL){
+                ans.push_back(cur->val);
+                cur=cur->right;
+            }
+            else{
+                TreeNode* prev=cur->left;
+                while(prev->right && prev->right!=cur){
+                    prev=prev->right;
+                }
+                if(prev->right==NULL){
+                    prev->right=cur;
+                    cur=cur->left;
+                }
+                else{
+                    prev->right=NULL;
+                    ans.push_back(cur->val);
+                    cur=cur->right;
+                }
+            }
+        }
         return ans;
     }
 };
