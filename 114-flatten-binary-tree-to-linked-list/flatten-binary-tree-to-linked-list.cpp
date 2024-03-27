245d57fae843743c87e49d3998dd175164225064
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2024-03-27 19:06:37 +0530

    Time: 10 ms (11.40%) | Memory: 15.7 MB (58.31%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -11,25 +11,32 @@
  */
 class Solution {
 public:
-    void pre_order(TreeNode* root,vector<int>&preorder){
-        if(root==NULL)return;
+    // void pre_order(TreeNode* root,vector<int>&preorder){
+    //     if(root==NULL)return;
 
-        preorder.push_back(root->val);
-        pre_order(root->left, preorder);
-        pre_order(root->right, preorder);
-    }
+    //     preorder.push_back(root->val);
+    //     pre_order(root->left, preorder);
+    //     pre_order(root->right, preorder);
+    // }
+    TreeNode * prev=NULL;
     void flatten(TreeNode* root) {
         if(root==NULL)return;
-        vector<int>preorder;
-        pre_order(root, preorder);
+        // vector<int>preorder;
+        // pre_order(root, preorder);
 
+        // root->left=NULL;
+        // root->right=NULL;
+        // TreeNode * temp=root;
+        // for(int i=1;i<preorder.size();i++){
+        //     temp->right= new TreeNode(preorder[i]);
+        //     temp=temp->right; 
+        // }
+        // temp->right=NULL;
+        flatten(root->right);
+        flatten(root->left);
+
+        root->right=prev;
         root->left=NULL;
-        root->right=NULL;
-        TreeNode * temp=root;
-        for(int i=1;i<preorder.size();i++){
-            temp->right= new TreeNode(preorder[i]);
-            temp=temp->right; 
-        }
-        temp->right=NULL;
+        prev=root;
     }
 };
