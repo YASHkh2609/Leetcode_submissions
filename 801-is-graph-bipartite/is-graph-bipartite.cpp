035931561d6e32909b2397215ae83168a24ea923
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2024-04-17 11:21:56 +0530

    Time: 16 ms (66.86%) | Memory: 16.1 MB (64.42%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,5 +1,17 @@
 class Solution {
 public:
+    bool dfs_check(int src,vector<vector<int>>& graph, vector<int>&coloured, int colour){
+        coloured[src]=colour;
+        for(int i=0;i<graph[src].size();i++){
+            if(coloured[graph[src][i]]==-1){
+                if(dfs_check(graph[src][i], graph, coloured, !coloured[src])==false)return false;
+            }
+            else{
+                if(coloured[graph[src][i]] == coloured[src])return false;
+            }
+        }
+        return true;
+    }
     bool bfs_check(int src,vector<vector<int>>& graph,vector<int>&coloured) {
         queue<int>q;
         q.push(src);
@@ -23,10 +35,10 @@ public:
     bool isBipartite(vector<vector<int>>& graph){
         int n=graph.size();
         vector<int>coloured(n,-1);
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){// we have to consider that graph might have components
             if(coloured[i]==-1){
-                coloured[i]=0;
-                if(!bfs_check(i, graph, coloured))return false;
+                //if(!bfs_check(i, graph, coloured))return false;//if any one component is not bipartite graph is not bipartite
+                if(!dfs_check(i,graph, coloured, 0))return false;
             }
         }
         return true;
