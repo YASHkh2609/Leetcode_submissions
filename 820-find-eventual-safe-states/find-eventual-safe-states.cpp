9870b400186d352726cd56f6bdd1486b579f62c3
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2024-04-24 12:14:14 +0530

    Time: 151 ms (28.32%) | Memory: 65.2 MB (33.44%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -25,19 +25,53 @@ public:
     }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n=graph.size();
-        vector<bool>safe_nodes(n,0);
-        vector<int>ans;
-        vector<int>vis(n,0);
-        vector<int>pathvis(n,0);
+        // vector<bool>safe_nodes(n,0);
+        // vector<int>ans;
+        // vector<int>vis(n,0);
+        // vector<int>pathvis(n,0);
 
+        // for(int i=0;i<n;i++){
+        //     int check = dfs(i, graph, vis, pathvis, safe_nodes);
+        // }
+        // for(int i=0;i<n;i++){
+        //     if(safe_nodes[i]==1){
+        //         ans.push_back(i);
+        //     }
+        // }
+        // return ans;
+
+        //using TOPO sort on reversed graph
+
+        //reverse the graph
+        vector<vector<int>>reversed_g(n);
         for(int i=0;i<n;i++){
-            int check = dfs(i, graph, vis, pathvis, safe_nodes);
-        }
-        for(int i=0;i<n;i++){
-            if(safe_nodes[i]==1){
-                ans.push_back(i);
+            for(auto it:graph[i]){
+                reversed_g[it].push_back(i);
             }
         }
-        return ans;
+        vector<int>indegree(n,0);
+        for(int i=0;i<n;i++){
+            for(auto it: reversed_g[i]){
+                indegree[it]++;
+            }
+        }
+
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0)q.push(i);
+        }
+        vector<int>safe_nodes;
+        while(!q.empty()){
+            int node= q.front();
+            q.pop();
+            safe_nodes.push_back(node);
+
+            for(auto it: reversed_g[node]){
+                indegree[it]--;
+                if(indegree[it]==0)q.push(it);
+            }
+        }
+        sort(safe_nodes.begin(), safe_nodes.end());
+        return safe_nodes;
     }
 };
