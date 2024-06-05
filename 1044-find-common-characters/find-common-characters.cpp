cfa930ea82b001ec096e7881bd702848b3b3dc44
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2024-06-05 19:37:40 +0530

    Time: 3 ms (96.29%) | Memory: 11.4 MB (69.23%) - LeetSync

--- 1044-find-common-characters/find-common-characters.cpp
+++ 1044-find-common-characters/find-common-characters.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     vector<string> commonChars(vector<string>& words) {
-        vector<vector<int>>hash_arr(words.size(), vector<int>(26,0));
+        vector<int>hash_arr(26, INT_MAX);
 
         for(int i=0;i<words.size();i++){
+            vector<int>temp(26,0);
             for(int j=0;j<words[i].length();j++){
-                hash_arr[i][words[i][j]-'a']++;
+                temp[words[i][j]-'a']++;
+            }
+            for(int k=0;k<26;k++){
+                hash_arr[k]=min(hash_arr[k], temp[k]);
             }
         }
+
         vector<string>ans;
-        for(int col=0;col<26;col++){
-            int mini=101;
-            for(int row=0;row<words.size();row++){
-                mini=min(mini, hash_arr[row][col]);
-            }
-            string s="";
-            s+= char(col+'a');
-            for(int k=0;k<mini;k++){
-                ans.push_back(s);
+        for(int i=0;i<26;i++){
+            for(int j=0;j<hash_arr[i];j++){
+                ans.push_back(string(1,i+'a'));
             }
         }
         return ans;
