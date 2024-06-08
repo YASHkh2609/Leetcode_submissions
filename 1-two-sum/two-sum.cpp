6c8e913cb1c8fb6cfdc243e86f7ca9323447537f
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2024-06-08 14:11:00 +0530

    Time: 18 ms (48.73%) | Memory: 18.3 MB (7.34%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,49 +1,32 @@
-
-#include<bits/stdc++.h>
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n=nums.size();
-        /*vector<int>nums1;
-        for(int k=0;k<nums.size();k++){
-            nums1.push_back(nums[k]);
-        }
-        sort(nums.begin(),nums.end());
-        int i=0;
-        int j=n-1;
-        vector<int> result;
-        int sum=0;
-        while(i<j){
-            sum=nums[i]+nums[j];
-            if(sum<target){
-                i++;
-            }
-            else if(sum>target){
-                j--;
-            }
-            else{
-                int left=binarySearch(nums1,0,n-1,nums[i]);
-                int right=binarySearch(nums1,0,n-1,nums[j]);
-                result.push_back(left);
-                result.push_back(right);
-                i++;
-                j--;
-                return result;
-            }
-        }
-        return result ;*/
-        vector<int>result;
-        for(int i=0;i<n;i++){
-            for(int k=i+1;k<n;k++){
-                int sum=nums[i]+nums[k];
-                if(sum==target){
-                    result.push_back(i);
-                    result.push_back(k);
-                    return result;
-                }
-            }
-        }
-        return result;
 
+        unordered_map<int, vector<int>>mpp;
+        for(int i=0;i<n;i++){
+            mpp[nums[i]].push_back(i);
+        }
+        vector<int>ans;
+
+        for(auto it:mpp){
+            int remaining=target-it.first;
+
+            if(remaining==it.first)
+            {
+                if(it.second.size()==2)
+                    return it.second;
+                else continue;
+            }
+
+            auto rem_it=mpp.find(remaining);
+            if(rem_it==mpp.end())continue;
+            else{
+                ans.push_back(it.second[0]);
+                ans.push_back(rem_it->second[0]);
+                break;
+            }
+        }
+        return ans;
     }    
 };
