e60711a2ce50efd2ad7cdf451130f7057848431c
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2024-06-11 14:21:22 +0530

    Time: 7 ms (5.98%) | Memory: 10.8 MB (6.31%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.cpp
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -3,17 +3,15 @@ public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
         unordered_map<int, int>mpp;
         for(int i=0;i<arr2.size();i++){
-            mpp[arr2[i]]=i;
+            mpp[arr2[i]]=i+1;
         }
-        int ind=arr2.size();
-        for(int i=0;i<arr1.size(); i++){
-            if(mpp.find(arr1[i])==mpp.end()){
-                mpp[arr1[i]]=ind;
-                ind++;
-            }
+
+        for(int i=0;i<arr1.size();i++){
+            if(mpp[arr1[i]]==0)mpp[arr1[i]]=1001;
         }
+
         auto compare = [&](int a, int b) { 
-            if(mpp[a]>=arr2.size() && mpp[b]>=arr2.size())return a<b;
+            if(mpp[a] == mpp[b])return a<b;
             return mpp[a] < mpp[b];  
         };
         sort(arr1.begin(), arr1.end(), compare);
