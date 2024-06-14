32abfd8e7f97a24e4267e3c43ae21f8ae091f06a
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2024-06-15 01:01:10 +0530

    Time: 90 ms (93.70%) | Memory: 68.7 MB (97.44%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
@@ -1,15 +1,30 @@
 class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int cnt=0;
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]<=nums[i-1]){
-                int new_num=nums[i-1]+1;
-                cnt+=new_num-nums[i];
-                nums[i]=new_num;
+        // sort(nums.begin(), nums.end());
+        // int cnt=0;
+        // for(int i=1;i<nums.size();i++){
+        //     if(nums[i]<=nums[i-1]){
+        //         int new_num=nums[i-1]+1;
+        //         cnt+=new_num-nums[i];
+        //         nums[i]=new_num;
+        //     }
+        // }
+        // return cnt;
+        int count[200001]={0};
+        // int maxi=0;
+        for(int i=0;i<nums.size();i++){
+            // maxi=max(maxi, nums[i]);
+            count[nums[i]]++;
+        }
+        int ans=0;
+        for(int i=0;i<200001;i++){
+            if(count[i]>1){
+                ans+=count[i]-1;
+                count[i+1]+=count[i]-1;
+                count[i]=1;
             }
         }
-        return cnt;
+        return ans;
     }
 };
