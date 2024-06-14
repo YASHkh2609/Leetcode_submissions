77ef16a0ca4d4b123aa71f1c2e53877a81d52b3a
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2024-06-14 13:58:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (39.69%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -1,26 +1,29 @@
 class MyStack {
     queue<int>q1;
-    queue<int>q2;
+    // queue<int>q2;
 public:    
     void push(int x) {
-        q2.push(x);
-        while(!q1.empty()){
-            q2.push(q1.front());
+        // q2.push(x);
+        // while(!q1.empty()){
+        //     q2.push(q1.front());
+        //     q1.pop();
+        // }
+        // while(!q2.empty()){
+        //     q1.push(q2.front());
+        //     q2.pop();
+        // }
+        q1.push(x);
+        for(int i=0;i<q1.size()-1;i++){
+            q1.push(q1.front());
             q1.pop();
         }
-        while(!q2.empty()){
-            q1.push(q2.front());
-            q2.pop();
-        }
     }
-    
     int pop() {
         int top=q1.front();
         q1.pop();
         return top;
 
-    }
-    
+    }  
     int top() {
         return q1.front();
     }
