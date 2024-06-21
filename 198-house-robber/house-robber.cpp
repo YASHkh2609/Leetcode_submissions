97d5b8b8014667c67f6312a4cb4479558bdeba50
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2024-06-21 15:25:55 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (60.07%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -16,15 +16,30 @@ public:
         int n=nums.size();
         vector<int>dp(n, -1);
         //return get_max_money(n-1,nums, dp);
-        dp[0]=nums[0];
-        for(int i=1;i<n;i++){
-            int left  = nums[i];
-            if(i>1)
-                left = nums[i] + dp[i - 2];//pick i
-            int right = dp[i-1];//not pick i
+        // dp[0]=nums[0];
+        // for(int i=1;i<n;i++){
+        //     int left  = nums[i];
+        //     if(i>1)
+        //         left+= dp[i - 2];//pick i
+        //     int right = dp[i-1];//not pick i
 
-            dp[i]=max(left, right);
+        //     dp[i]=max(left, right);
+        // }
+        // return dp[n-1];
+
+        int prev=nums[0];
+        int prev2=0;
+
+        for(int i= 1;i<n;i++){
+            int left = nums[i];
+            if(i>1)left+=prev2;
+            int right = prev;
+
+            int cur =max(left, right);
+
+            prev2=prev;
+            prev=cur;
         }
-        return dp[n-1];
+        return prev;
     }
 };
