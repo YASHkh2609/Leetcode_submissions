e24f55e5b6438dc5045e6d1197236c5fac36f380
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2024-06-21 14:51:49 +0530

    Time: 4 ms (31.43%) | Memory: 9.3 MB (35.12%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,19 +1,19 @@
 class Solution {
 private:
-    int get_max_money(int ind, int n, vector<int>& nums, vector<int>&dp ){
-        if(ind>=n)return 0;
+    int get_max_money(int ind, vector<int>& nums, vector<int>&dp ){//memoization
+        if(ind<0)return 0;
         if(dp[ind]!=-1)return dp[ind];
         //pick
-        int left = nums[ind] + get_max_money(ind + 2, n , nums, dp);
+        int left = nums[ind] + get_max_money(ind - 2, nums, dp);
         //not pick 
-        int right = get_max_money(ind + 1, n , nums, dp);
+        int right = get_max_money(ind - 1, nums, dp);
 
         return dp[ind] = max(left, right);
     }
 public:
-    int rob(vector<int>& nums) {
+    int rob(vector<int>& nums){ 
         int n=nums.size();
         vector<int>dp(n, -1);
-        return get_max_money(0,n,nums, dp);
+        return get_max_money(n-1,nums, dp);
     }
 };
