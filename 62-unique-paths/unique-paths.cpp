295f513c76667453d07791d409dafd08ed51491d
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2024-07-05 19:56:14 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (72.29%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -4,30 +4,46 @@ private:
         if(row==0 && col == 0)return 1;
 
         if(dp[row][col]!=-1)return dp[row][col];
-        int way1=0, way2=0;
+        int up=0, left=0;
 
-        if(row-1>=0) way1 = get_paths(row-1, col, dp);
-        if(col-1>=0) way2 = get_paths(row, col-1, dp);
+        if(row-1>=0) up = get_paths(row-1, col, dp);
+        if(col-1>=0) left = get_paths(row, col-1, dp);
 
-        return dp[row][col] = way1+way2;
+        return dp[row][col] = up+left;
     }
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m, vector<int>(n,-1));
+        // vector<vector<int>>dp(m, vector<int>(n,-1));
         // return get_paths(m-1,n-1, dp);
-        for(int i=0;i<n;i++)dp[0][i]=1;
-        for(int i=0;i<m;i++)dp[i][0]=1;
 
-        for(int row=1;row<m;row++){
-            for(int col=1;col<n;col++){
-                int way1=0, way2=0;
-                if(row-1>=0) way1 = dp[row-1][col];
-                if(col-1>=0) way2 = dp[row][col-1];
+        // for(int row=0;row<m;row++){
+        //     for(int col=0;col<n;col++){
+        //         if(row==0 && col==0)dp[row][col]=1;
+        //         else{
+        //             int up=0, left=0;
+        //             if(row-1>=0) up = dp[row-1][col];
+        //             if(col-1>=0) left = dp[row][col-1];
 
-                dp[row][col] = way1+ way2;
+        //             dp[row][col] = up+ left;
+        //         }
+        //     }
+        // }
+        // return dp[m-1][n-1];
+
+        //Space Optimisation
+
+        vector<int>dp(n,0);
+        for(int row=0;row<m;row++){
+            vector<int>temp(n,0);
+            for(int col=0;col<n;col++){
+                if(row==0 && col==0)temp[col]=1;
+                else{
+                    if(col-1>=0)temp[col]= dp[col]+temp[col-1];
+                    else temp[col]=dp[col];
+                }
             }
+            dp=temp;
         }
-        return dp[m-1][n-1];
-
+        return dp[n-1];
     }
 };
