2baf088a31e219d576d9a23e94f6a1eae91bf839
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2024-07-29 11:40:19 +0530

    Time: 521 ms (46.19%) | Memory: 219.1 MB (20.85%) - LeetSync

--- 2171-second-minimum-time-to-reach-destination/second-minimum-time-to-reach-destination.cpp
+++ 2171-second-minimum-time-to-reach-destination/second-minimum-time-to-reach-destination.cpp
@@ -1,50 +1,46 @@
 class Solution {
 public:
     int secondMinimum(int n, vector<vector<int>>& edges, int time, int change) {
-        vector<vector<int>> adj(n + 1);
-        for (auto& edge: edges) {
-            adj[edge[0]].push_back(edge[1]);
-            adj[edge[1]].push_back(edge[0]);
+        vector<vector<int>>adj(n+1);
+        for(auto it:edges){
+            int u=it[0];
+            int v=it[1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
         }
 
-        // dist1[i] stores the minimum time taken to reach node i from node 1. dist2[i] stores the
-        // second minimum time taken to reach node from node 1. freq[i] stores the number of times a
-        // node is popped out of the heap.
-        vector<int> dist1(n + 1, numeric_limits<int>::max()),
-            dist2(n + 1, numeric_limits<int>::max()), freq(n + 1);
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> min_heap;
-        min_heap.push({0, 1});
-        dist1[1] = 0;
+        queue<pair<int , int>>q;
+        vector<vector<int>>dist(n+1,vector<int>(2, INT_MAX));
 
-        while (!min_heap.empty()) {
-            auto [timeTaken, node] = min_heap.top();
-            min_heap.pop();
+        q.push({1, 0});
+        dist[1][0]=0;
 
-            freq[node]++;
-            // If the node is being visited for the second time and is 'n', return the answer.
-            if (freq[node] == 2 && node == n) return timeTaken;
-            // If the current light is red, wait till the path turns green.
-            if ((timeTaken / change) % 2) {
-                timeTaken = change * (timeTaken / change + 1) + time;
-            } else {
-                timeTaken = timeTaken + time;
+        while(!q.empty()){
+            int node=q.front().first;
+            int d=q.front().second;
+            q.pop();
+            int new_dist=0;
+            int signal = d/change; //to see if signal green or red
+            if(signal %2==0){//green
+                new_dist= d+time;
+            }
+            else{//red
+                int waitTime=change - d%change;
+                new_dist = d+waitTime+time;
             }
 
-            for (auto& neighbor: adj[node]) {
-                // Ignore nodes that have already popped out twice.
-                if (freq[neighbor] == 2) continue;
-                // Update dist1 if it's more than the current timeTaken and store its value in
-                // dist2 since that becomes the second minimum value now.
-                if (dist1[neighbor] > timeTaken) {
-                    dist2[neighbor] = dist1[neighbor];
-                    dist1[neighbor] = timeTaken;
-                    min_heap.push({timeTaken, neighbor});
-                } else if (dist2[neighbor] > timeTaken && dist1[neighbor] != timeTaken) {
-                    dist2[neighbor] = timeTaken;
-                    min_heap.push({timeTaken, neighbor});
+            for(auto it:adj[node]){
+                if(dist[it][0]==INT_MAX){//first visit
+                    q.push({it, new_dist});
+                    dist[it][0]=new_dist;
+                }
+                else if(dist[it][1]==INT_MAX && new_dist>dist[it][0]){//second visit
+                    q.push({it, new_dist});
+                    dist[it][1]=new_dist;
                 }
             }
         }
-        return 0;
+        return dist[n][1];
     }
 };
