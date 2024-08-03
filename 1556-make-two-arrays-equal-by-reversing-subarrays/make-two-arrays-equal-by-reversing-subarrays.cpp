a2d131984c5ba5de649680be0261c862ab885919
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2024-08-04 00:57:40 +0530

    Time: 12 ms (34.04%) | Memory: 19.7 MB (20.91%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
-        unordered_map<int, int>mpp1;
-        unordered_map<int, int>mpp2;
+        // unordered_map<int, int>mpp1;
+        // unordered_map<int, int>mpp2;
 
+        // for(int i=0;i<arr.size();i++){
+        //     mpp1[arr[i]]++;
+        // }
+        // for(int j=0;j<target.size();j++){
+        //     mpp2[target[j]]++;
+        // }
+
+        // for(auto it:mpp1){
+        //     if(mpp2[it.first]==0 || mpp2[it.first]!=mpp1[it.first])return false;
+        // }
+        // return true;
+
+        unordered_map<int, int>mpp;
         for(int i=0;i<arr.size();i++){
-            mpp1[arr[i]]++;
-        }
-        for(int j=0;j<target.size();j++){
-            mpp2[target[j]]++;
+            mpp[arr[i]]++;
         }
 
-        for(auto it:mpp1){
-            if(mpp2[it.first]==0 || mpp2[it.first]!=mpp1[it.first])return false;
+        for(int j=0;j<target.size();j++){
+            if(mpp.find(target[j])==mpp.end())return false;
+            mpp[target[j]]--;
+            if(mpp[target[j]]==0)mpp.erase(target[j]);
         }
         return true;
     }
