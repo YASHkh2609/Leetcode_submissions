86cd118be24028ed1e0a0e1ceac428bb6dac10d0
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2024-08-05 20:18:52 +0530

    Time: 10 ms (13.58%) | Memory: 23.3 MB (15.86%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -3,17 +3,30 @@ public:
     string reverseWords(string s) {
         string temp="";
         string ans="";
+        stack<char>st;
         for(int i=0;i<s.length();i++){
-            if(s[i]==' ' && temp.length()!=0){
-                if(ans.length()==0)ans+=temp;
-                else ans = temp + " " + ans;
+            if(s[i]==' '){
+                while(!st.empty()){
+                    temp=st.top()+temp;
+                    st.pop();
+                }
+                if(temp!=""){
+                    if(ans.length()==0)ans = temp+ans;
+                    else ans =temp + " "+ans;
+                }
                 temp="";
             }
-            else if(s[i]!=' ')temp+=s[i];
+            else{
+                st.push(s[i]);
+            }
         }
-        if(temp.length()!=0){
-            if(ans.length()==0)ans+=temp;
-            else ans = temp + " " + ans;
+        while(!st.empty()){
+                temp=st.top()+temp;
+                st.pop();
+            }
+        if(temp!=""){
+            if(ans.length()==0)ans = temp+ans;
+            else ans =temp + " "+ans;
         }
         return ans;
     }
