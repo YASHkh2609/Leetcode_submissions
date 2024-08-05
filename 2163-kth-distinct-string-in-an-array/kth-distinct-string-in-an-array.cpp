a3b110cb3615bbb156deed33dc92b73a8c85e160
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2024-08-05 14:56:17 +0530

    Time: 19 ms (42.71%) | Memory: 18.5 MB (20.16%) - LeetSync

--- 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.cpp
+++ 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     string kthDistinct(vector<string>& arr, int k) {
         int cnt_distinct=0;
+        unordered_map<string, int>mpp;
         for(int i=0;i<arr.size();i++){
-            int cnt=1;
-            for(int j=0;j<arr.size();j++){
-                if(i!=j && arr[i]==arr[j])cnt++;
-            }
-            if(cnt==1)cnt_distinct++;
+            mpp[arr[i]]++;
+        }
+        for(int i=0;i<arr.size();i++){
+            if(mpp[arr[i]]==1)cnt_distinct++;
             if(cnt_distinct==k)return arr[i];
         }
         return "";
