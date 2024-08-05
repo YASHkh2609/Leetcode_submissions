ff8b81af30775abf341a8d37bf4d41ed9a2db600
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2024-08-05 15:51:55 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (62.61%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,15 +1,33 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        string temp="";
-        int cnt=0;
-        while(cnt<s.length()){
-            temp=s.substr(1, s.length()-1);
-            temp+=s[0];
-            if(temp==goal)return true;
-            s=temp;
-            cnt++;
+        // string temp="";
+        // int cnt=0;
+        // while(cnt<s.length()){
+        //     temp=s.substr(1, s.length()-1);
+        //     temp+=s[0];
+        //     if(temp==goal)return true;
+        //     s=temp;
+        //     cnt++;
+        // }
+        // return false;
+        if(s.length()!=goal.length())return false;
+        if(s.length()==0)return true;
+        string copy = s+s;
+        int i=0, j=0, k=0;
+        while(i<copy.length() && k<goal.length()){
+            if(copy[j]==goal[k]){
+                j++;
+                k++;
+            }
+            else{
+                i++;
+                j=i;
+                k=0;
+            }
         }
+        if(k==goal.length())return true;
         return false;
+
     }
 };
