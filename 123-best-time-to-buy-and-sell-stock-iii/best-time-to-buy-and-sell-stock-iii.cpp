520e839576fe53a262b2205abe46dbf8047202a8
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2024-08-24 16:31:01 +0530

    Time: 438 ms (46.30%) | Memory: 209.8 MB (51.90%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -16,20 +16,22 @@ private:
 public:
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>>dp(n+1, vector<vector<int>>(2, vector<int>(3,-1)));
+        // vector<vector<vector<int>>>dp(n+1, vector<vector<int>>(2, vector<int>(3,-1)));
         // return f(0, 1, 2, prices,dp);
         //base case
         //ind==n
-        for(int j=0;j<=1;j++){
-            for(int k=0;k<=2;k++){
-                dp[n][j][k]=0;
-            }
-        }
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=1;j++){
-                dp[i][j][0]=0;
-            }
-        }
+        vector<vector<vector<int>>>dp(n+1, vector<vector<int>>(2, vector<int>(3,0)));
+        //above we have initialised our dp with 0 so we dont need to write base case
+        // for(int j=0;j<=1;j++){
+        //     for(int k=0;k<=2;k++){
+        //         dp[n][j][k]=0;
+        //     }
+        // }
+        // for(int i=0;i<=n;i++){
+        //     for(int j=0;j<=1;j++){
+        //         dp[i][j][0]=0;
+        //     }
+        // }
 
         for(int ind=n-1;ind>=0;ind--){
             for(int buy=0;buy<=1;buy++){
