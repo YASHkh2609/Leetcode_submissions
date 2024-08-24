aef5e51f168c8ed0d610662ec31192e1360351e7
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2024-08-24 18:15:26 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (70.55%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -17,23 +17,42 @@ public:
         int n=prices.size();
         // vector<vector<int>>dp(n+1,vector<int>(2,-1));
         // return f(0, 1, prices, dp);
-        vector<vector<int>>dp(n+2,vector<int>(2,-1));
-        dp[n][0]=0;
-        dp[n][1]=0;
-        dp[n+1][0]=0;
-        dp[n+1][1]=0;
+        // vector<vector<int>>dp(n+2,vector<int>(2,-1));
+        // dp[n][0]=0;
+        // dp[n][1]=0;
+        // dp[n+1][0]=0;
+        // dp[n+1][1]=0;
+        // for(int ind=n-1;ind>=0;ind--){
+        //     for(int buy=0;buy<=1;buy++){
+        //         int profit=0;
+        //         if(buy){
+        //             profit=max(-prices[ind]+dp[ind+1][0], 0+dp[ind+1][1]);
+        //         }
+        //         else{ //cant buy -- sell/ not sell
+        //             profit = max(prices[ind]+dp[ind+2][1], 0+dp[ind+1][0]);
+        //         }
+        //         dp[ind][buy]=profit;
+        //     }
+        // }
+        // return dp[0][1];
+        vector<int>prev1(2,0);//ind+1
+        vector<int>prev2(2,0);//ind+2
+
         for(int ind=n-1;ind>=0;ind--){
+            vector<int>temp(2,-1);
             for(int buy=0;buy<=1;buy++){
                 int profit=0;
                 if(buy){
-                    profit=max(-prices[ind]+dp[ind+1][0], 0+dp[ind+1][1]);
+                    profit=max(-prices[ind]+prev1[0], 0+prev1[1]);
                 }
                 else{ //cant buy -- sell/ not sell
-                    profit = max(prices[ind]+dp[ind+2][1], 0+dp[ind+1][0]);
+                    profit = max(prices[ind]+prev2[1], 0+prev1[0]);
                 }
-                dp[ind][buy]=profit;
+                temp[buy]=profit;
             }
+            prev2=prev1;
+            prev1=temp;
         }
-        return dp[0][1];
+        return prev1[1];
     }
 };
