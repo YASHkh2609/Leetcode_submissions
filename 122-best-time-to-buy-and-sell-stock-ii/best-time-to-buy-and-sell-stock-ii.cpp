4addba14cea5e30abe9bd3a6de243d9cb61e3c5d
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2024-08-24 15:14:36 +0530

    Time: 6 ms (52.27%) | Memory: 20.5 MB (11.55%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,75 +1,22 @@
-// class Solution {
-// private:
-//     int getmaxProfit(int ind, int buy, vector<int>& prices, vector<vector<int>>&dp){
-//         if(ind == prices.size()){
-//             return 0;
-//         }
-//         if(dp[ind][buy]!=-1)return dp[ind][buy];
-//         int profit=0;
-//         if(buy){ // i'm not holding a stock
-//             profit = max(-prices[ind]+getmaxProfit(ind+1, 0, prices,dp), 0+getmaxProfit(ind+1, 1, prices, dp));
-//         }
-//         else{//holding a stock
-//             profit = max(prices[ind]+getmaxProfit(ind+1, 1, prices,dp),0+getmaxProfit(ind+1, 0, prices, dp));
-//         }
-//         return dp[ind][buy] = profit;
-//     }
-// public:
-//     int maxProfit(vector<int>& prices) {
-//         int n=prices.size();
-//         // vector<vector<int>>dp(n, vector<int>(2, -1));
-//         // return getmaxProfit(0, 1, prices, dp);
-//         // vector<vector<int>>dp(n+1, vector<int>(2, -1));
-
-//         // dp[n][0]=0;
-//         // dp[n][1]=0;
-
-//         // for(int ind=n-1;ind>=0;ind--){
-//         //     for(int buy=0;buy<=1;buy++){
-//         //         int profit=0;
-//         //         if(buy){ // i'm not holding a stock
-//         //             profit = max(-prices[ind]+dp[ind+1][0], 0+dp[ind+1][1]);
-//         //         }
-//         //         else{//holding a stock
-//         //             profit = max(prices[ind]+dp[ind+1][1],0+dp[ind+1][0]);
-//         //         }
-//         //         dp[ind][buy] = profit;
-//         //     }
-//         // }
-//         // return dp[0][1];
-
-//         // vector<int>dp(2, -1);
-//         // dp[0]=0;
-//         // dp[1]=0;
-//         // for(int ind=n-1;ind>=0;ind--){
-//         //     vector<int>temp(2,-1);
-//         //     for(int buy=0;buy<=1;buy++){
-//         //         int profit=0;
-//         //         if(buy){ // i'm not holding a stock
-//         //             profit = max(-prices[ind]+dp[0], 0+dp[1]);
-//         //         }
-//         //         else{//holding a stock
-//         //             profit = max(prices[ind]+dp[1],0+dp[0]);
-//         //         }
-//         //         temp[buy] = profit;
-//         //     }
-//         //     dp=temp;
-//         // }
-//         // return dp[1];
-        
-
-//     }
-// };
-
 class Solution {
+private:
+    int f(int ind, bool holding_stock, vector<int>& prices,vector<vector<int>>&dp){
+        if(ind == prices.size())return 0;
+        if(dp[ind][holding_stock]!=-1)return dp[ind][holding_stock];
+        int profit=0;
+        if(holding_stock){
+            profit = max(prices[ind]+f(ind+1, 0,prices,dp), 0+f(ind+1,1,prices,dp));
+        }
+        else{ //not holding stock
+            profit =max(-prices[ind]+f(ind+1, 1,prices,dp), 0+f(ind+1, 0,prices,dp));
+        }
+        return dp[ind][holding_stock] = profit;
+    }
 public:
     int maxProfit(vector<int>& prices) {
-        int diff = 0;
-        for(int i=0;i<prices.size()-1;i++){
-            if(prices[i+1] > prices[i]){
-                diff += prices[i+1] - prices[i];
-            }
-        }
-        return diff;
+        //dp[n+1][2]
+        int n=prices.size();
+        vector<vector<int>>dp(n+1,vector<int>(2,-1));
+        return f(0, 0, prices, dp);
     }
 };
