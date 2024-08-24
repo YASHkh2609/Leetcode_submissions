e6a6654e2557f7d57def4404231c41c1d2479b6c
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2024-08-24 17:59:32 +0530

    Time: 227 ms (65.04%) | Memory: 120 MB (67.68%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -20,44 +20,44 @@ public:
         int n=prices.size();
         // vector<vector<int>>dp(n+1, vector<int>(4, -1));
         // return f(0, 0,prices,dp);
-        vector<vector<int>>dp(n+1, vector<int>(5, 0));
+        // vector<vector<int>>dp(n+1, vector<int>(5, 0));
         // initialising dp array with 0 so that we dont have to write
         // base case 
         // base case --> dp[n][..]=0, dp[..][2k]=0;
+        // for(int ind=n-1;ind>=0;ind--){
+        //     for(int num_trans=0;num_trans<4;num_trans++){
+        //         int profit=0;
+        //         if(num_trans%2==0){ //num_trans - even ==> buy
+        //             profit=max(-prices[ind]+dp[ind+1][num_trans+1],
+        //                 0+dp[ind+1][num_trans]);
+        //         }
+        //         else{// num_trans - odd ==> sell
+        //             profit =max(prices[ind]+dp[ind+1][num_trans+1],
+        //                 0+dp[ind+1][num_trans]);
+        //         }
+        //         dp[ind][num_trans]=profit;
+        //     }
+        // }
+        // return dp[0][0];
+    
+        //SPACE OPTIMIZATION
+        vector<int>dp(5, 0);
         for(int ind=n-1;ind>=0;ind--){
+            vector<int>temp(5, 0);
             for(int num_trans=0;num_trans<4;num_trans++){
                 int profit=0;
                 if(num_trans%2==0){ //num_trans - even ==> buy
-                    profit=max(-prices[ind]+dp[ind+1][num_trans+1],
-                        0+dp[ind+1][num_trans]);
+                    profit=max(-prices[ind]+dp[num_trans+1],
+                        0+dp[num_trans]);
                 }
                 else{// num_trans - odd ==> sell
-                    profit =max(prices[ind]+dp[ind+1][num_trans+1],
-                        0+dp[ind+1][num_trans]);
+                    profit =max(prices[ind]+dp[num_trans+1],
+                        0+dp[num_trans]);
                 }
-                dp[ind][num_trans]=profit;
+                temp[num_trans]=profit;
             }
+            dp=temp;
         }
-        return dp[0][0];
-    
-        //SPACE OPTIMIZATION
-        // vector<int>dp(2*k+1, 0);
-        // for(int ind=n-1;ind>=0;ind--){
-        //     vector<int>temp(2*k+1, 0);
-        //     for(int num_trans=0;num_trans<2*k;num_trans++){
-        //         int profit=0;
-        //         if(num_trans%2==0){ //num_trans - even ==> buy
-        //             profit=max(-prices[ind]+dp[num_trans+1],
-        //                 0+dp[num_trans]);
-        //         }
-        //         else{// num_trans - odd ==> sell
-        //             profit =max(prices[ind]+dp[num_trans+1],
-        //                 0+dp[num_trans]);
-        //         }
-        //         temp[num_trans]=profit;
-        //     }
-        //     dp=temp;
-        // }
-        // return dp[0];
+        return dp[0];
     }
 };
