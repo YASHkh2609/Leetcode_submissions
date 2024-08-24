1144509831d8b0b6629dead6867b6de07225472e
YASHkh2609 <116063681+YASHkh2609@users.noreply.github.com>
2024-08-24 18:24:03 +0530

    Time: 159 ms (36.38%) | Memory: 80 MB (64.63%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -16,22 +16,39 @@ private:
 public:
     int maxProfit(vector<int>& prices, int fee) {
         int n=prices.size();
-        vector<vector<int>>dp(n+1,vector<int>(2,-1));
+        // vector<vector<int>>dp(n+1,vector<int>(2,-1));
         // return f(0, 1,fee, prices, dp);
-        dp[n][0]=0;
-        dp[n][1]=0;
+        // dp[n][0]=0;
+        // dp[n][1]=0;
+        // for(int ind=n-1;ind>=0;ind--){
+        //     for(int buy=0;buy<=1;buy++){
+        //         int profit=0;
+        //         if(buy){
+        //             profit=max(-prices[ind]+dp[ind+1][0], 0+dp[ind+1][1]);
+        //         }
+        //         else{ //cant buy -- sell/ not sell
+        //             profit = max(prices[ind]-fee+dp[ind+1][1], 0+dp[ind+1][0]);
+        //         }
+        //         dp[ind][buy]=profit;
+        //     }
+        // }
+        // return dp[0][1];
+        //SPACE OPTIMISATION
+        vector<int>dp(2,0);
         for(int ind=n-1;ind>=0;ind--){
+            vector<int>temp(2,-1);
             for(int buy=0;buy<=1;buy++){
                 int profit=0;
                 if(buy){
-                    profit=max(-prices[ind]+dp[ind+1][0], 0+dp[ind+1][1]);
+                    profit=max(-prices[ind]+dp[0], 0+dp[1]);
                 }
                 else{ //cant buy -- sell/ not sell
-                    profit = max(prices[ind]-fee+dp[ind+1][1], 0+dp[ind+1][0]);
+                    profit = max(prices[ind]-fee+dp[1], 0+dp[0]);
                 }
-                dp[ind][buy]=profit;
+                temp[buy]=profit;
             }
+            dp=temp;
         }
-        return dp[0][1];
+        return dp[1];
     }
 };
